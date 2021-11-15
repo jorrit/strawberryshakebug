f91a30782fe411e0044485fab399ab11995a8cb5
Jorrit Schippers <jorrit@ncode.nl>
2021-11-15 16:50:17 +0100

    Add project files.

---
+++ strawberryshakebug/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+//using strawberryshakebug.Demo;
+
+namespace strawberryshakebug
+{
+    public class HomeController : Controller
+    {
+        private readonly IConferenceClient _client;
+
+        public HomeController(IConferenceClient client)
+        {
+            _client = client;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var result = await _client.GetSessions.ExecuteAsync();
+
+            result.EnsureNoErrors();
+
+            return View(result.Data.Sessions);
+        }
+    }
+}
